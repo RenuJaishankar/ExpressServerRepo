40c9de542b5f3613515d6d4f9573a0b9a3149237
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-02-11 11:35:23 -0500

    fixes port

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const port = process.env.PORT || 3000
 let apples= {name:'apples',amount:4}
 app.use(function(req, res, next) {
