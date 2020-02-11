ddd429bb646f782b62b169c4307ee921d8f7db26
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-02-11 11:32:14 -0500

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const port = process.env.PORT || 3000
 let apples= {name:'apples',amount:4}
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
